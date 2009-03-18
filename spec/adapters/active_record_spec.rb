640cb907eddddcfe9952ede6c3c7e70aab58f323
Sander Hartlage <sander@outside.in>
2009-03-18 01:12:36 -0400

    Added the proper requires into the adapter specs

--- spec/adapters/active_record_spec.rb
+++ spec/adapters/active_record_spec.rb
@@ -1,4 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'active_record'
+require 'enygma/adapters/active_record'
 
 describe Enygma::Adapters::ActiveRecordAdapter do
   
--- spec/adapters/sequel_spec.rb
+++ spec/adapters/sequel_spec.rb
@@ -1,4 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'sequel'
+require 'enygma/adapters/sequel'
 
 describe Enygma::Adapters::SequelAdapter do
   
