03af2ecb4e6cd725ba6247e77281021e18db686b
Sander Hartlage <sander@outside.in>
2009-03-18 00:39:05 -0400

    Wrote BasicObject spec

--- spec/extensions/basic_object_spec.rb
+++ spec/extensions/basic_object_spec.rb
@@ -1,1 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Enygma::BasicObject do
+  
+  it "should have no instance methods except for __id__ and __send__" do
+    supermethods = Enygma::BasicObject.ancestors.inject([]) do |methods, mod|
+      methods += mod.instance_methods
+    end.uniq
+    methods = Enygma::BasicObject.instance_methods - supermethods
+    methods.sort == %w{ __id__ __send__ }
+  end
+  
+end
