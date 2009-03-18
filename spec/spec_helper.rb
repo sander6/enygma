df0066e2954e620aa10d909fb5ce519095f04076
Sander Hartlage <sander@outside.in>
2009-03-17 23:35:52 -0400

    Added a helper module to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,22 @@
 require 'rubygems'
-require 'spec'
 require File.expand_path(File.dirname(__FILE__) + '/../lib/enygma')
+require 'spec'
 require 'mocha'
+
+module SpecHelpers
+  
+  def stub_sphinx!
+    Enygma::Search.any_instance.stubs(:__query_sphinx__).returns({})
+  end
+  
+  def stub_database!
+    Enygma::Search.any_instance.stubs(:__query_database__).returns({})
+  end
+  
+end
 
 Spec::Runner.configure do |config|
   config.mock_with :mocha
+  config.include(SpecHelpers)
 end
+
