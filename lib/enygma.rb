7183cfafde22a55c6b08be4c0b473be7f4515240
Sander Hartlage <sander@outside.in>
2009-04-23 10:54:24 -0400

    Changed requires and the way the configuration object is saved in included classes

--- lib/enygma.rb
+++ lib/enygma.rb
@@ -1,30 +1,30 @@
-require 'riddle'
-
 $:.unshift File.dirname(__FILE__)
 
 require 'api/sphinx'
 
 require 'enygma/version'
 
-require 'enygma/extensions/basic_object'
-require 'enygma/extensions/enumerable'
 require 'enygma/extensions/float'
 
 require 'enygma/adapters/abstract_adapter'
 require 'enygma/configuration'
+require 'enygma/geodistance_proxy'
 require 'enygma/search'
+
+require 'enygma/resource'
 
 module Enygma
   
   def self.included(base)
-    base.instance_variable_set(:@enygma_configuration, Enygma::Configuration.new)
+    # base.instance_variable_set(:@enygma_configuration, Enygma::Configuration.new)
+    base.const_set(:"#{base.name.gsub(/(?!^)([A-Z])/, '_\1').upcase}_ENYGMA_CONFIGURATION", Enygma::Configuration.new)
     base.__send__(:extend, Enygma::ClassMethods)
   end
   
   module ClassMethods
     
     def enygma_configuration
-      self.instance_variable_get(:@enygma_configuration)
+      self.const_get(:"#{self.name.gsub(/(?!^)([A-Z])/, '_\1').upcase}_ENYGMA_CONFIGURATION")
     end
     
     def configure_enygma(&config)
@@ -32,7 +32,7 @@ module Enygma
     end
     
     def search(*tables)
-      Enygma::Search.new(@enygma_configuration, :tables => tables)
+      Enygma::Search.new(enygma_configuration, :tables => tables)
     end
     
   end
