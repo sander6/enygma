4a8e174b1ef8c43d7a197b8f0710f51a3d4f218a
Sander Hartlage <sander@outside.in>
2009-04-23 10:53:41 -0400

    Removed BasicObject and EnumerableExtensions

--- lib/enygma/extensions/basic_object.rb
+++
@@ -1,9 +0,0 @@
-module Enygma
-  if defined? BasicObject and RUBY_VERSION >= '1.9'
-    Enygma::BasicObject = ::BasicObject
-  else
-    class BasicObject
-      instance_methods.each { |m| undef_method m unless %w{ __id__ __send__ }.include?(m) }
-    end
-  end
-end
