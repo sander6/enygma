12ab7d1167a5f98b7f2d26b61c029f56853ff3af
Sander Hartlage <sander@outside.in>
2009-04-28 12:58:05 -0400

    Rewrote Sequel adapter

--- lib/enygma/adapters/sequel.rb
+++ lib/enygma/adapters/sequel.rb
@@ -5,22 +5,46 @@ module Enygma
     
     class SequelAdapter < Enygma::Adapters::AbstractAdapter
       
-      # def connect!(datastore)
-      #   @datastore = case datastore
-      #   when Sequel::Model
-      #     @table = datastore.table_name
-      #     datastore.db
-      #   end
-      # end
-      # 
-      # def query(args = {})
-      #   @datastore[args[:datastore]].filter(:id => args[:ids])
-      # end
-      # 
-      # def get_attribute(record, attribute)
-      #   record[attribute]
-      # end
+      class InvalidTable < StandardError; end
       
+      def connect!(datastore)
+        @datastore = case datastore
+        when Sequel::Model
+          @table = datastore.table_name
+          datastore.db
+        when Sequel::Database
+          datastore
+        when :sqlite
+          Sequel.sqlite
+        else
+          Sequel.connect(datastore)
+        end
+      end
+      
+      def query(args = {})
+        get_table(args[:table]).filter(:id => args[:ids])
+      end
+      
+      def get_attribute(record, attribute)
+        record[attribute]
+      end
+      
+      private
+      
+      def get_table(obj = nil)
+        case obj
+        when Symbol
+          @datastore ? @datastore[obj] : raise(InvalidTable)
+        when String
+          @datastore ? @datastore[obj.to_sym] : raise(InvalidTable)
+        when Sequel::Model
+          obj
+        when Sequel::Database
+          @table ? obj[@table] : raise(InvalidTable)
+        else
+          raise InvalidTable
+        end
+      end
     end
     
   end
